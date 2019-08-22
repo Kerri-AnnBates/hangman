2135c49b227c13f3ed22c17c7f9d0d12917ff0fb
Kerri-Ann <kaybee1jam@yahoo.com>
2019-08-22 12:19:26 -0400

    took off http protocol from api request

--- scripts/request.js
+++ scripts/request.js
@@ -1,6 +1,6 @@
 //Make an HTTP request
 const getPuzzle = async (wordCount) => {
-  const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+  const response = await fetch(`//puzzle.mead.io/puzzle?wordCount=${wordCount}`)
   if (response.status === 200) {
     const data = await response.json()
     return data.puzzle
